b328ca22066e0acbd7932840329491d5daf9c7bc
cainzuca <caiomelosantos99@gmail.com>
2024-06-19 23:05:26 -0300

    puxando os dados do titulo

--- coleta/coleta/spiders/mercadolivre.py
+++ coleta/coleta/spiders/mercadolivre.py
@@ -4,7 +4,14 @@ import scrapy
 class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     allowed_domains = ["lista.mercadolivre.com.br"]
-    start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
+    start_urls = ["https://lista.mercadolivre.com.br/celular-recondicionado#D[A:celular%20recondicionado]"]
 
     def parse(self, response):
-        pass
+
+        bloco = response.css('div.ui-search-result__wrapper')
+        
+        for bloco in bloco:
+
+            yield {
+                'titulo' : bloco.css('h2.ui-search-item__title::text').get()
+            }               
